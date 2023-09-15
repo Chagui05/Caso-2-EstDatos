e76e3cb00a0dcc22fd3138d82b1cccbe849571e1
Santiago Chaves <santichavesg@gmail.com>
2023-09-14 23:03:24 -0600

    annadiendo clase bathroom

---
+++ Bathroom.h
@@ -0,0 +1,30 @@
+#ifndef _BATHROOM_
+#define _BATHROOM_ 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Bathroom {
+private: 
+    int ID;
+    bool occupied;
+    int countBaths;
+public:
+    Bathroom(){
+        ID = 402+countBaths;
+        occupied = false;
+        countBaths++
+    }
+    void setOccupied(){
+        occupied = true;
+    }
+    bool getOccupied(){
+        return occupied;
+    }
+    int getID(){
+        return ID;
+    }
+    
+};
+
+#endif
