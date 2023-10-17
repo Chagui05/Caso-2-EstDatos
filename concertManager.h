f9c75b1005bb2bb107e8477041333b8a0aa03d72
Santiago Chaves <santichavesg@gmail.com>
2023-10-17 10:54:55 -0600

    concertMAnager y exitManager arreglados

--- concertManager.h
+++ concertManager.h
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include <thread>
-#include "json.hpp"
 #include "jsonManager.h"
-#include "ConcertEntrance.h"
 #include "queueManager.h"
 
 using namespace std;
@@ -12,83 +12,62 @@ using json = nlohmann::json;
 class concertManager
 {
 private:
-
-    QueueManager* queueManager;
-    float proccessDuration; //sacarlo del json
+    QueueManager *queueManager;
+    float proccessDuration; // sacarlo del json
     float velocidadSalidaSort;
-    double posibilidadGramilla;
-    double posibilidadBanno; 
+    double posibilidadBanno;
     double posibilidadTienda;
-    int cantidadBannos;
-    int cantidadStore;
 
 public:
-
-    concertManager(QueueManager* pQueueManager)
+    concertManager(QueueManager *pQueueManager)
     {
         queueManager = pQueueManager;
-        proccessDuration = 2.0;//cambiar y que se saque del json 
+        proccessDuration = 2.0; // cambiar y que se saque del json
         Config config = Config("config.json");
         velocidadSalidaSort = config.velocidadSalidaSort;
-        posibilidadGramilla = config.posibilidadGramilla;
         posibilidadBanno = config.posibilidadBanno;
         posibilidadTienda = config.posibilidadTienda;
-        cantidadStore = config.cantidadDeStore;
-        cantidadBannos = config.cantidadDeBannos;
     }
 
-    // será un hilo
     void takeToBath()
-    {   
-        AttenderGroup* pAttenderGroup = audienciaPrincipal->takeFromWaitingQueue();
-        
-        int divisorSubgrupos = pAttenderGroup->getSize()/cantidadBannos
-        int subgrupoSize = pAttenderGroup->getSize() / numSubgrupos;
-        vector<AttenderGroup> subgrupos;
-
-        for (int i = 0; i<divisorSubgrupos;i++){
-            subgrupos.push_back(new AttenderGroup(subgrupoSize));
-        }
-
-        for (int i = 0; !subgrupos.empty();i++){
-            queueManager->addToBath(subgrupos.back());
-            subgrupos.pop_back();
-        //std::this_thread::sleep_for(std::chrono::seconds(velocidadEntrada));
-        }
+    {
+        AttenderGroup *grupo = queueManager->getAudienceArea()->takeFromWaitingStack();
+        queueManager->addQuantityToBath(grupo->getSize()); // logica manejada en funcion hilo de QueueManager
     }
+
     void addToAudienciaFromBath()
     {
-        for(int i = 0; i < queueManager->getBathroom()->size(); i++){
-            AttenderGroup* grupo = queueManager->getBathroom()->at(i).takeFromWaitingQueue();
-            queueManager->getAudienceArea()->addToWaitingStack(grupo);
-            //std::this_thread::sleep_for(std::chrono::seconds(velocidadSalidaSort));
-        }   
+        queueManager->bathToAudiencia(); // logica manejada en funcion hilo de QueueManager
     }
+
     void takeToStore()
     {
-        AttenderGroup* pAttenderGroup = audienciaPrincipal->takeFromWaitingQueue();
-
-        // Calcular el tamaño de cada subgrupo
-        int divisorSubgrupos = pAttenderGroup->getSize()/cantidadStore
-        int subgrupoSize = pAttenderGroup->getSize() / numSubgrupos;
-        vector<AttenderGroup> subgrupos;
-
-        for (int i = 0; i<divisorSubgrupos;i++){
-            subgrupos.push_back(new AttenderGroup(subgrupoSize));
-        }
-
-        for (int i = 0; !subgrupos.empty();i++){
-            queueManager->addToStore(subgrupos.back());
-            subgrupos.pop_back();
-        //std::this_thread::sleep_for(std::chrono::seconds(velocidadEntrada));
-        }
+        AttenderGroup *grupo = queueManager->getAudienceArea()->takeFromWaitingStack();
+        queueManager->addQuantityToStore(grupo->getSize()); // logica manejada en funcion hilo de QueueManager
     }
+
     void addToAudienciaFromStore()
     {
-        for(int i = 0; i < queueManager->getStore()->size(); i++){
-            AttenderGroup* grupo = queueManager->getStore()->at(i).takeFromWaitingQueue();
-            queueManager->getAudienceArea()->addToWaitingStack(grupo);
-            //std::this_thread::sleep_for(std::chrono::seconds(velocidadSalidaSort));
-        }   
+        queueManager->storeToAudiencia(); // logica manejada en funcion hilo de QueueManager
+    }
+
+    void sortToBathOrStore() // deber ser un hilo, usar el atributo velocidadSalidaSort
+    {
+        std::srand(std::time(0));
+        double aleatorio = static_cast<double>(std::rand()) / RAND_MAX;
+
+        if (aleatorio < posibilidadBanno)
+        {
+            takeToBath();
+        }
+        else
+        {
+            takeToStore();
+        }
+    }
+
+    QueueManager* getQueueManager()
+    {
+        return queueManager;
     }
 };
--- exitManager.h
+++ exitManager.h
@@ -1,47 +1,45 @@
-#ifndef _EXITMANAGER_ 
+#ifndef _EXITMANAGER_
 #define _EXITMANAGER_ 1
 #include <string>
 #include <thread>
 #include "listas/List.h"
 #include "attender.h"
-#include "AudienceArea.h"
+#include "queueManager.h"
 
 using namespace std;
 
 class exitManager
 {
 private:
-
     bool timeToExit;
-    AudienceArea* audienciaASalir;
+    QueueManager *queueManager;
     int cantidadDeGrupos;
+    List<AttenderGroup> *personasSalidas;
 
 public:
-    
-        exitManager(AudienceArea* pAudienciaASalir)
-        {
-            timeToExit = false;
-            audienciaASalir = pAudienciaASalir;
-        }
-    
-        
-        //hilo
-        void exit()
-        {
-            if (timeToExit != true){
-                return;
-            }
-            else{
-                while (audienciaASalir->getEmpty() == false)
-                {
-                    for (int i = 0; i < audienciaASalir->getWaitingStack()->getSize(); i++)
-                    {
-                        audienciaASalir->takeFromWaitingStack();
-                    }
-                }
-            }
-        }
+    exitManager(QueueManager *pQueueManager)
+    {
+        timeToExit = false;
+        queueManager = pQueueManager;
+        personasSalidas = new List<AttenderGroup>();
+    }
 
+    // hilo
+    List<AttenderGroup>* exit()
+    {
+        if (timeToExit == true)
+        {
+            while (queueManager->getAudienceArea()->getWaitingStack()->getSize() > 0)
+            {
+                personasSalidas->enqueue(queueManager->getAudienceArea()->takeFromWaitingStack());
+            }
+        }
+        return personasSalidas;
+    }
+    void setTrueExit()
+    {
+        timeToExit = true;
+    }
 };
 
 #endif
